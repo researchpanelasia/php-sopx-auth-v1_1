09bf301d266c46689ecea671cdbc41ddfdb4cd33
yowcowvg <yoko_ohyama@voyagegroup.com>
2014-09-05 20:10:08 +0900

    implemented

--- lib/SOPx_Auth_V1_1.class.php
+++ lib/SOPx_Auth_V1_1.class.php
@@ -19,4 +19,49 @@ class SOPx_Auth_V1_1 {
 
     public function getAppId() { return $this->app_id; }
     public function getAppSecret() { return $this->app_secret; }
+
+    public function generateSignature($query) {
+
+        if (!array_key_exists('time', $query) || !$query['time']) {
+            throw new InvalidArgumentException('Missing required parameter: time');
+        }
+
+        ksort($query);
+        $params = array();
+        foreach ($query as $key => $val) {
+            $params[] = "{$key}={$val}";
+        }
+        return hash_hmac(
+                'sha256',
+                implode('&', $params),
+                $this->getAppSecret()
+                );
+    }
+
+    public function signQuery($query) {
+
+        if (!array_key_exists('time', $query)) {
+            $query['time'] = time();
+        }
+
+        return array_merge(
+            $query,
+            array('sig' => $this->generateSignature($query))
+        );
+    }
+
+    public function verifyQuery($query) {
+
+        if (!array_key_exists('sig', $query)) {
+            return false;
+        }
+        if (!array_key_exists('time', $query)) {
+            return false;
+        }
+
+        $sig = $query['sig'];
+        unset($query['sig']);
+
+        return $sig === $this->generateSignature($query);
+    }
 }
--- tests/SOPx_Auth_V1_1Test.php
+++ tests/SOPx_Auth_V1_1Test.php
@@ -7,6 +7,12 @@ require_once('lib/SOPx_Auth_V1_1.class.php');
 
 class SOPx_Auth_V1_1Test extends PHPUnit_Framework_TestCase {
 
+    protected $auth;
+
+    protected function setUp() {
+        $this->auth = new SOPx_Auth_V1_1('1234', 'hogefuga');
+    }
+
     /**
      * @expectedException   InvalidArgumentException
      */
@@ -26,5 +32,114 @@ class SOPx_Auth_V1_1Test extends PHPUnit_Framework_TestCase {
 
         $this->assertEquals('1', $auth->getAppId());
         $this->assertEquals('hogehoge', $auth->getAppSecret());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testGenerateSignature_without_time() {
+        $this->auth->generateSignature(array(
+            'hoge' => 'hoge',
+            'fuga' => 'fuga',
+        ));
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testGenerateSignature_with_blank_time() {
+        $this->auth->generateSignature(array(
+            'hoge' => 'hoge',
+            'fuga' => 'fuga',
+            'time' => '',
+        ));
+    }
+
+    public function testGenerateSignature() {
+        $sig = $this->auth->generateSignature(array(
+            'app_id' => '1234',
+            'hoge' => 'hoge',
+            'fuga' => 'fuga',
+            'time' => '123456',
+        ));
+
+        $this->assertEquals('c4a23f5cb57d0bfe16a7ad4fc58ba0cccd92122efb144b0abf43315ff171a746', $sig);
+    }
+
+    public function testSignQuery_without_time() {
+        $res = $this->auth->signQuery(array(
+            'app_id' => '1234',
+            'hoge' => 'hoge',
+            'fuga' => 'fuga',
+        ));
+
+        $this->assertArrayHasKey('sig', $res);
+        $this->assertArrayHasKey('time', $res);
+    }
+
+    public function testSignQuery_with_time() {
+        $res = $this->auth->signQuery(array(
+            'app_id' => '1234',
+            'hoge' => 'hoge',
+            'fuga' => 'fuga',
+            'time' => '123456',
+        ));
+
+        $this->assertSame(
+            array(
+                'app_id' => '1234',
+                'hoge' => 'hoge',
+                'fuga' => 'fuga',
+                'time' => '123456',
+                'sig' => 'c4a23f5cb57d0bfe16a7ad4fc58ba0cccd92122efb144b0abf43315ff171a746',
+            ),
+            $res
+        );
+    }
+
+    public function testVerifyQuery_without_time() {
+        $res = $this->auth->verifyQuery(array(
+            'app_id' => '1234',
+            'hoge' => 'hoge',
+            'fuga' => 'fuga',
+            'sig' => 'c4a23f5cb57d0bfe16a7ad4fc58ba0cccd92122efb144b0abf43315ff171a746',
+        ));
+
+        $this->assertFalse($res);
+    }
+
+    public function testVerifyQuery_without_sig() {
+        $res = $this->auth->verifyQuery(array(
+            'app_id' => '1234',
+            'hoge' => 'hoge',
+            'fuga' => 'fuga',
+            'time' => '123456',
+        ));
+
+        $this->assertFalse($res);
+    }
+
+    public function testVerifyQuery_fail() {
+        $res = $this->auth->verifyQuery(array(
+            'app_id' => '1234',
+            'hoge' => 'hogehoge',
+            'fuga' => 'fugafuga',
+            'time' => '123456',
+            'sig' => 'c4a23f5cb57d0bfe16a7ad4fc58ba0cccd92122efb144b0abf43315ff171a746',
+        ));
+
+        $this->assertFalse($res);
+    }
+
+    public function testVerifyQuery_succeed() {
+        $res = $this->auth->verifyQuery(array(
+            'app_id' => '1234',
+            'hoge' => 'hoge',
+            'fuga' => 'fuga',
+            'time' => '123456',
+            'sig' => 'c4a23f5cb57d0bfe16a7ad4fc58ba0cccd92122efb144b0abf43315ff171a746',
+        ));
+
+        $this->assertTrue($res);
     }
 }
