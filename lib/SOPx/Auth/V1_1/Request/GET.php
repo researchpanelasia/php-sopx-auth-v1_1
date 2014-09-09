cffc0babe939db62c2638432da08013ac23de3a8
yowcowvg <yoko_ohyama@voyagegroup.com>
2014-09-09 20:33:28 +0900

    implemented GET request maker

--- lib/SOPx/Auth/V1_1/Request/GET.php
+++ lib/SOPx/Auth/V1_1/Request/GET.php
@@ -2,6 +2,25 @@
 
 namespace SOPx\Auth\V1_1\Request;
 
+use \SOPx\Auth\V1_1\Util;
+use \Httpful\Request;
+
 class GET
 {
+    public static function createRequest(\Net_URL2 $uri, array $params, $app_secret)
+    {
+        if (!array_key_exists('time', $params) || !$params['time']) {
+            throw new \InvalidArgumentException('Missing required parameter "time" in params');
+        }
+        if (!$app_secret) {
+            throw new \InvalidArgumentException('Missing app_secret');
+        }
+
+        $uri->setQueryVariables(array_merge(
+            $params,
+            array( 'sig' => Util::createSignature($params, $app_secret), )
+        ));
+
+        return Request::get($uri->getURL());
+    }
 }
