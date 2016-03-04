4683c6aa84860086238d724b9f6201465b2ad966
yowcow <yowcow@cpan.org>
2016-03-04 07:54:56 +0000

    update POST-JSON

--- tests/SOPx-Auth-V1_1-Request-POSTJSONTest.php
+++ tests/SOPx-Auth-V1_1-Request-POSTJSONTest.php
@@ -1,6 +1,6 @@
 <?php
 
-use \Net_URL2;
+use \GuzzleHttp\Psr7\Uri;
 use \SOPx\Auth\V1_1\Util;
 use \SOPx\Auth\V1_1\Request\POSTJSON;
 
@@ -8,7 +8,7 @@ class SOPx_Auth_V1_1_Request_POSTJSONTest extends \PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-        $this->uri = new \Net_URL2('http://www.researchpanelasia.com');
+        $this->uri = new Uri('http://www.researchpanelasia.com');
     }
 
     /**
@@ -47,22 +47,25 @@ class SOPx_Auth_V1_1_Request_POSTJSONTest extends \PHPUnit_Framework_TestCase
             'hogehoge'
         );
 
-        $uri = new \Net_URL2($req->uri);
+        $uri = $req->getUri();
 
-        $this->assertEquals('POST', $req->method);
-        $this->assertEquals($this->uri->getURL(), $uri->getURL());
-        $this->assertEquals($req->content_type, 'application/json');
+        $this->assertEquals('POST', $req->getMethod());
+        $this->assertEquals("http://www.researchpanelasia.com", $uri . "");
+        $this->assertEquals($req->getHeader('content-type'), array('application/json'));
         $this->assertEquals(
             array(
                 'aaa' => 'aaa',
                 'bbb' => 'bbb',
                 'time' => '1234',
             ),
-            json_decode($req->payload, true)
+            json_decode($req->getBody(), true)
         );
+
+        $sig = $req->getHeader('X-Sop-Sig');
+
         $this->assertEquals(
-            Util::createSignature($req->payload, 'hogehoge'),
-            $req->headers['X-Sop-Sig']
+            Util::createSignature($req->getBody() . "", 'hogehoge'),
+            $sig[0]
         );
     }
 }
